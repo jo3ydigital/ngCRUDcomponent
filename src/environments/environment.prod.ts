56007ac9306b935eaa4a1535491484e07cf9e28e
jo3ydigital <joey@joeydigital.net>
2020-05-31 21:45:56 -0400

    updated environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,4 @@ export const environment = {
   css_base_path: 'https://www.joeydigital.net/examples/crud-superheroes/assets', 
   api_endpoint_crud: 'https://www.joeydigital.net/examples/crud-superheroes-php/crud/index.php',
   api_endpoint_search: 'https://www.joeydigital.net/examples/crud-superheroes-php/search/index.php'
-
-  // base_url: 'http://www.joeydigital.net/examples/crud-superheroes',
-  // css_base_path: 'http://www.joeydigital.net/examples/crud-superheroes/assets', 
-  // api_endpoint_crud: 'http://www.joeydigital.net/examples/crud-superheroes-php/crud/index.php',
-  // api_endpoint_search: 'http://www.joeydigital.net/examples/crud-superheroes-php/search/index.php'
 };
